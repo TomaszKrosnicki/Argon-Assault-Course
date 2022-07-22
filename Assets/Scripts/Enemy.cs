9161d88c383658bca4cefec9bba872fd2017f8bc
Tomasz Krośnicki <krosnickitomek@gmail.com>
2022-07-22 09:11:50 +0200

    94. Public Methods In Unity C#

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,7 +6,26 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+
+    ScoreBoard scoreBoard;
+
+    void Start() 
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
     void OnParticleCollision(GameObject other) 
+    {
+        ProcessHit();
+        KillEnenmy();
+    }
+
+    void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    void KillEnenmy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
